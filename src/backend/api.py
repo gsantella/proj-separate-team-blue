e5540d97bb3f4339f3d962e437a656d97e170f23
Gavin Black <gavin.matthew.black@gmail.com>
2020-10-07 19:36:00 +0000

    Did the vercel thing for the backend

--- src/backend/api.py
+++ src/backend/api.py
@@ -1,16 +1,10 @@
 from flask import Flask
 from flask import json
-from flask_cors import CORS
 from flask import request
 
-
-#                        WIP 
-
-
 app = Flask('app')
-cors = CORS(app)
 
-@app.route('/f')
+@app.route('/api/f')
 def convertToC():
   fTemp = request.args['fTemp']
   cTemp = (int(fTemp) - 32) * 5/9
@@ -20,7 +14,7 @@ def convertToC():
   }
   return json.jsonify(data)
 
-@app.route('/c')
+@app.route('/api/c')
 def convertToF():
   cTemp = request.args['cTemp']
   fTemp = (int(cTemp) * 9/5) + 32
@@ -29,5 +23,3 @@ def convertToF():
   "Fahrenheit " : fTemp
   }
   return json.jsonify(data)
-
-app.run(host='0.0.0.0', port=8080)
